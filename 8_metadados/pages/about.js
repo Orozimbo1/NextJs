9f12f7fd39e319b9612ff835fb90482cae7b38cf
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-04-18 21:16:41 -0300

    Metatags

---
+++ 8_metadados/pages/about.js
@@ -0,0 +1,16 @@
+import Head from "next/head"
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Sobre nós</title>
+      </Head>
+      <div>
+        <h1>Sobre nós</h1>
+      </div>
+    </>
+  )
+}
+
+export default About
